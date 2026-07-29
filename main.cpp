ef25fbfb5276ebbcc9d60e7371b3cd027083b453
privateice <dejahvu.too@gmail.com>
2026-07-29 17:23:30 -0400

    removed Manager and HR classes, not needed

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #pragma once
 #include "Employee.h"
-#include "Manager.h"
-#include "HR.h"
 #include <iostream>
 
 int main() {
-    HR hr;
-    Manager manager;
-    hr.work();
-    manager.work();
     std::cout << "Company simulation running." << std::endl;
+    while(true)
+        {
+            
+
+        }
+
     return 0;
 }
