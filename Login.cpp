a2a22d2984208bb20e5d648df7b2c7809081e745
privateice <dejahvu.too@gmail.com>
2026-07-30 17:04:56 -0400

    Moved CheckPassword and Find user to Employee and EmployeesList

--- Login.cpp
+++ Login.cpp
@@ -1,34 +1,16 @@
+#include <iostream>
 #include "Login.h"
-#include "Employees.h"
-
-Login::Login(std::vector<Employee> employees)
-{
-   
-};
+#include "Employee.h"
+#include "EmployeesList.h"
 
 
-int FindUser(std::string username, std::vector<Employee> employees)
-{
-    // loop through the employees
-    for (size_t i = 0; i < employees.size(); i++)
-    {   // look for the username and return the index
-        if (employees[i].GetUsername == username) {return (i)}
-    }
-    return(-1) //return -1 if not found
-};
-
-int CheckPassword(std::string pwd, Employee emp)
-{   // check if the password matches: true if yes, false if no
-    std::hash<std::string> hasher;
-    if (hasher(pwd) == emp.GetPassword()) {return(1);}
-    return(0);
-};
-
-int ProcessLogin( std::vector<Employee> employees,
+int ProcessAuthentication( EmployeesList employees,
                     std::string username, 
                     std::string pwd) 
 {
-    i = FindUser(username, employees);
-    if (i >= 0 ) {return(CheckPassword(pwd,employees[i]))};
-    else (return(-1))
+    int userindex = employees.FindUser(username);
+    if (userindex > -1 ) {
+        return(employees[userindex].CheckPassword(pwd));
+    };
+    return(-1);
 };
--- Login.h
+++ Login.h
@@ -6,16 +6,11 @@
 class Login
 {
     private:
-        int FindUser(std::string username, 
-                    std::vector<Employee> employees) const;
-        int CheckPassword(std::string pwd, Employee emp) const;
+
     
     public:
-        int ProcessLogin( std::vector<Employee> employees,
-                                std::string username, 
-                                std::string pwd
-                                ) const;
-        Login(std::vector<Employee> employees);
-
+        int ProcessAuthentication(EmployeesList employees,
+                std::string username, std::string pwd) const;
+        Login(){};
 
 };
